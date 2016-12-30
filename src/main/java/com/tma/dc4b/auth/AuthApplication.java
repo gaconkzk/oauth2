a6f103de5888333ba646273fefbdf8664fe25813
tntvu <tntvu@tma.com.vn>
2016-12-30 15:01:11 +0700

    oauth2 and shit

--- src/main/java/com/tma/dc4b/auth/AuthApplication.java
+++ src/main/java/com/tma/dc4b/auth/AuthApplication.java
@@ -1,12 +1,61 @@
 package com.tma.dc4b.auth;
 
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  *
  */
 @SpringBootApplication
+@RestController
+@EnableAuthorizationServer
+@EnableResourceServer
 public class AuthApplication extends WebMvcConfigurerAdapter {
+
+  @GetMapping("/user")
+  public Principal user(Principal user) {
+    return user;
+  }
+
+  @Configuration
+  @Order(-20)
+  protected  static class LoginConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+          .formLogin()
+          .and()
+            .requestMatchers()
+            .antMatchers("/login","/oauth/authorize", "/oauth/confirm_access")
+          .and()
+            .authorizeRequests()
+              .anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth.parentAuthenticationManager(authenticationManager);
+    }
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthApplication.class, args);
+  }
 
 }
