556838bd5494e8ad73aec868da56ea6a6458323c
tntvu <tntvu@tma.com.vn>
2016-12-30 13:24:10 +0700

    new commit

---
+++ src/main/java/com/tma/dc4b/auth/AuthApplication.java
@@ -0,0 +1,12 @@
+package com.tma.dc4b.auth;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class AuthApplication extends WebMvcConfigurerAdapter {
+
+}
